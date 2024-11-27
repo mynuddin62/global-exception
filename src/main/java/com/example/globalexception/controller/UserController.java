08bf0a8fd9f78a1100a8c4594945a17eea3948b9
mynuddin62 <mynuddinratul@gmail.com>
2024-11-27 14:02:12 +0600

    [Adding Logger -> slf4j logger]

--- src/main/java/com/example/globalexception/controller/UserController.java
+++ src/main/java/com/example/globalexception/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.globalexception.controller;
 
 import com.example.globalexception.exception.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/users/{id}")
     public String getUser(@PathVariable String id) {
         if ("123".equals(id)) {
             return "User Found";
         } else {
+            logger.error("User not found with ID: {}", id);
             throw new ResourceNotFoundException("User with ID " + id + " not found");
         }
     }
